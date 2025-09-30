0d2ef4ec602c05248ad1972e6c39da17493cfcae
FoxxMD <FoxxMD@users.noreply.github.com>
2025-09-30 17:46:45 +0000

    update

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
 import './index.css'
 import { Room } from './pages/Room'
 import { Root } from './pages/Root'
